ba99a788d437c44e23533da8f135779b65e2bda3
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2024-05-16 23:48:31 -0300

    remove create product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { OrderService } from './order/order.service';
-import { MagentoController } from './magento/magento.controller';
 
 import { OrderController } from './order/order.controller';
 import { MagentoService } from './magento/magento.service';
 
 @Module({
   imports: [HttpModule],
-  controllers: [OrderController, MagentoController],
+  controllers: [OrderController],
   providers: [OrderService, MagentoService],
 })
 export class AppModule {}
